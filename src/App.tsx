68675a8ce51c3d07e94c293f0df64fc88bdc3d4f
joaoth <joaopedromcj9@gmail.com>
2023-06-19 18:03:16 -0300

    ba

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
+  const [values, setValues] = useState();
+
+  const handleClickButton = () => {
+    console.log(values)
+  }
+
+  const handleChangeValues = (value:any) => {
+    setValues((prevValue: any) => ({ 
+      ...prevValue,
+      [value.target.name]: value.target.value,
+
+    }))
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <div className="container">
+        <h1>Scrim games</h1>
+        <input type="text" placeholder="Nome" name="name" onChange={handleChangeValues}/>
+        <input type="text" placeholder="Preço" name="cost"onChange={handleChangeValues}/> 
+        <input type="text" placeholder="Categoria" name="category" onChange={handleChangeValues}/>   
+        <button onClick={handleClickButton}>Cadastrar</button>
+      </div> 
+    </div>
   )
 }
 
