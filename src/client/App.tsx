2ea2761d0d2eb6ec38491129bcdc88d7121788ec
joaoth <joaopedromcj9@gmail.com>
2023-06-26 17:55:13 -0300

    aa

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -5,28 +5,15 @@ import Card from '../components/cards/card';
 import { Pagination } from '@mui/material';
 
 const App = () => {
-  const [text, setText] = useState('');
   const [message, setMessage] = useState('');
   const [values, setValues] = useState<any>();
   const [listGames, setListGames] = useState<any>([])
   const [currentPage, setCurrentPage] = useState(1);
-<<<<<<< HEAD
   const itemsPerPage = 10;
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = listGames.slice(indexOfFirstItem, indexOfLastItem);
 
-
-=======
-  const itemsPerPage = 12;
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  {
-    typeof listGames !== "undefined" && listGames.map((value: string | any) => {
-      const currentItems = listGames.slice(indexOfFirstItem, indexOfLastItem);
-    })
-  }
->>>>>>> 6bdec1e88957271328a1651195580dd042e1a0b1
 
   const handlePageChange = (event: React.ChangeEvent<unknown>, page: number) => {
     setCurrentPage(page);
@@ -102,17 +89,15 @@ const App = () => {
 
         <h1 className="register-title ">lojinha gamer</h1>
 
-        <h1 className="register-title ">jotinha games</h1>
-
         <input className='register-input' type="text" placeholder="Nome" name="name" onChange={handleChangeValues} />
         <input className='register-input' type="text" placeholder="Preço" name="cost" onChange={handleChangeValues} />
         <input className='register-input' type="text" placeholder="Categoria" name="category" onChange={handleChangeValues} />
         <button className="register-button" onClick={handleRegisterGame}>Cadastrar</button>
       </div>
 
-      
-      <input type="text" onChange={handleChangeValues}/> <button onClick={handleSearchGames}>VAI</button>
-     
+
+      <input type="text" onChange={handleChangeValues} /> <button onClick={handleSearchGames}>VAI</button>
+
       {currentItems.map((val: any) => (
         <Card
           listCard={listGames}
@@ -124,19 +109,6 @@ const App = () => {
           category={val.category}
         />
       ))}
-
-      {typeof listGames !== "undefined" && listGames.map((value: string | any) => {
-        return <Card
-          key={value.id}
-          listCard={listGames}
-          setListCard={setListGames}
-          id={value.id}
-          name={value.name}
-          cost={value.cost}
-          category={value.category}
-        ></Card>
-      })}
-
       <Pagination
         count={Math.ceil(listGames.length / itemsPerPage)}
         shape="rounded"
